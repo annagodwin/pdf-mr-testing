ea5a080e14314abf3c84f6b927307dc9853f10c3
Anna Godwin <anna.godwin17@gmail.com>
2022-12-12 11:08:44 -0500

    Simplify function

--- pdfmrtesting/pdfminersix.py
+++ pdfmrtesting/pdfminersix.py
@@ -1,23 +1,14 @@
 from pdfminer.high_level import extract_text
 from pathlib import Path
-from utils import determine_result, timeout
+from utils import determine_result
 
-@timeout(5)
-def lib_extract_text(pdf_filepath: Path): 
 
+def test_extract_text(pdf_filepath):
     try:
         text = extract_text(pdf_filepath).strip()
         
         result = determine_result(text)
-    
-    except Exception as e:
-        result = e
-    
-    return result
-
-def test_extract_text(pdf_filepath):
-    try:
-        result = lib_extract_text(pdf_filepath)
+        
     except Exception as e:
         result = e
     return result
